69b5f8830406567d62ac847d0f1eaebf4c70bed4
donald <donald.tangze@outlook.com>
2017-12-07 21:20:16 +1100

    Q21修改

---
+++ src/Question21/MergeLinkedList.java
@@ -0,0 +1,42 @@
+package Question21;
+
+import question2.ListNode;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+/**
+ * Created by Administrator on 2017/12/7.
+ */
+public class MergeLinkedList {
+    /*public LinkedList merge(LinkedList l1, LinkedList l2){
+        ListIterator<Integer> iterator1 = l1.listIterator();
+        ListIterator<Integer> iterator2 = l2.listIterator();
+        LinkedList<Integer> resultList = new LinkedList<>();
+        while(iterator1.next() < iterator2.next()){
+            resultList.addFirst(iterator1.next());
+            return
+        }
+            iterator1.next()
+    }*/
+
+    public ListNode merge2(ListNode l1, ListNode l2){
+        if(null != l1 && null == l2) {
+            return l1;
+        }
+        if(null == l1 && null != l2){
+            return l2;
+        }
+        if(null == l1 && null == l2){
+            return null;
+        }
+        if(l1.getValue() <= l2.getValue()){
+            l1.setNext(merge2(l1.getNext(), l2));
+            return l1;
+        }else if(l1.getValue() > l2.getValue()){
+            l2.setNext(merge2(l1, l2.getNext()));
+            return l2;
+        }
+        return null;
+    }
+}
---
+++ src/test/Question21/MergeLinkedListTest.java
@@ -0,0 +1,41 @@
+package test.Question21; 
+
+import Question21.MergeLinkedList;
+import org.junit.Test;
+import org.junit.Before; 
+import org.junit.After;
+import question2.ListNode;
+
+/** 
+* MergeLinkedList Tester. 
+* 
+* @author donald
+* @since <pre>12/07/2017</pre> 
+* @version 1.0 
+*/ 
+public class MergeLinkedListTest {
+
+/** 
+* 
+* Method: merge2(ListNode l1, ListNode l2) 
+* 
+*/ 
+@Test
+public void testMerge2() throws Exception {
+    ListNode listNode1 = new ListNode(2);
+    listNode1.setNext(new ListNode(4));
+
+    ListNode listNode2 = new ListNode(5);
+    listNode2.setNext(new ListNode(8));
+
+    MergeLinkedList mergeLinkedList= new MergeLinkedList();
+    ListNode listNode3 = mergeLinkedList.merge2(listNode1, listNode2);
+
+    while(null != listNode3){
+        System.out.print(listNode3.getValue());
+        listNode3 = listNode3.getNext();
+    }
+} 
+
+
+} 
